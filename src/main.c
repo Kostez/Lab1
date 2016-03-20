d2a9d76cbed295567bd9337accab7cff8ef6489a
Kostez <kostelom@bk.ru>
2016-03-21 03:40:02 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <getopt.h>
 
 int main(int argc, char *argv[]) {
+	
+	const char* short_options = "m:a::s::p::";
 	const char* modes[] = {"std", "child", "posix", "kill", "pipe"};
 	
 	const struct option long_options[] = {
@@ -9,7 +11,39 @@ int main(int argc, char *argv[]) {
 		{"signal",optional_argument,NULL,'s'},
 		{"pid",optional_argument,NULL,'p'},
 	};
-  
+	
+	int rez;
+	int option_index;
+	
+	while ((rez=getopt_long(argc,argv,short_options,
+		long_options,&option_index))!=-1){
+			switch(rez){
+			case 'm': {
+				printf("This is demo help. Try -h or --help.\n");
+				printf("option_index = %d (\"%s\",%d,%c)\n",
+					option_index,
+					long_options[option_index].name,
+					long_options[option_index].has_arg,
+					long_options[option_index].val
+				);
+				break;
+			};
+			case 'a': {
+				
+				break;
+			};
+			case 's': {
+				
+				break;
+			};
+			case 'p': {
+				
+				break;
+			};
+			
+			
+			
+		}
   
   
   
