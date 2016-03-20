8e14aa53aff771276208e6eb42e28a5e3d98c9c7
Kostez <kostelom@bk.ru>
2016-03-21 02:56:54 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char *argv[]) {
   
+  const struct option long_options[] = {
+        {"opta",no_argument,&flag_a,1},
+        {"optb",no_argument,&flag_b,10},
+        {"optc",no_argument,&flag_c,-121},
+        {NULL,0,NULL,0}
+    };
   
   
   
