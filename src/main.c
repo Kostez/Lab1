91d0d0eadd37392dd811621ce4804f5716d5f477
Kostez <kostelom@bk.ru>
2016-03-21 03:21:55 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,13 @@
 #include <getopt.h>
 
 int main(int argc, char *argv[]) {
-  
-  const char* modes[] = {"std", "child", "posix", "kill", "pipe"};
-  
-  const struct option long_options[] = {
-		{"help",no_argument,NULL,'h'},
-		{"size",optional_argument,NULL,'s'},
-		{"file",required_argument,NULL,'f'},
-		{NULL,0,NULL,0}
+	const char* modes[] = {"std", "child", "posix", "kill", "pipe"};
+	
+	const struct option long_options[] = {
+		{"mode",required_argument,NULL,'m'},
+		{"amount",optional_argument,NULL,'a'},
+		{"signal",optional_argument,NULL,'s'},
+		{"pid",optional_argument,NULL,'p'},
 	};
   
   
