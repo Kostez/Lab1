75653c9d1de2835a8ef966c69383ce4107c2dca4
Kostez <kostelom@bk.ru>
2016-03-23 00:03:25 +0600

    Update child.c

--- src/child.c
+++ src/child.c
@@ -3,9 +3,10 @@
 void handler_child_mode(int signum, siginfo_t *info, void *f){
         printf("\nНомер сигнала: %d\n", info->si_signo);
         printf("Значение errno: %d\n", info->si_errno);
-        printf("Значение errno: %d\n", info->si_errno);
-        printf("Значение errno: %d\n", info->si_errno);
-        printf("Значение errno: %d\n", info->si_errno);
+        printf("Код сигнала: %d\n", info->si_code);
+        printf("Идентификатор процесса, пославшего сигнал: %d\n", info->si_pid);
+        printf("Реальный идентификатор пользователя процесса, пославшего сигнал: %d\n", info->si_uid);
+        printf("Реальный идентификатор пользователя процесса, пославшего сигнал: %d\n", info->si_uid);
         return;
 }
 
