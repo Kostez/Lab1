7f637d23d7bf5f057449a4aff436187d867767b8
Kostez <kostelom@bk.ru>
2016-03-23 04:51:55 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -1,24 +1,18 @@
 #include <general.h>
 
+int signal_c = 0;
+
 void handler_posix_mode(int signum, siginfo_t *info, void *f){
-        fprintf(stderr, "\nNumber of signal is %d\nPID of process is %d\nGID of process is %d\n\n", signum, info->si_pid, getpgid(info->si_pid));
+        
+        fprintf(stderr, " %i | %i | %i | %i | %i\n", signal_c, info->si_pid, getpgid(), signal, );
+        signal_c++;
         return;
 }
 
 void mode_posix(int amount){
-        struct sigaction std_s;
-        std_s.sa_sigaction = handler_std_mode;
-        std_s.sa_flags = SA_SIGINFO;
-        if(sigaction(SIGUSR1, &std_s, 0) == -1){
-                perror(NULL);
-                exit(1);
-        }
-        if(sigaction(SIGUSR2, &std_s, 0) == -1){
-                perror(NULL);
-                exit(1);
-        }
-        if(sigaction(SIGHUP, &std_s, 0) == -1){
-                perror(NULL);
-                exit(1);
-        }
+        struct sigaction posix_s;
+        posix_s.sa_sigaction = handler_posix_mode;
+        posix_s.sa_flags = SA_SIGINFO;
+        
+        
 }
