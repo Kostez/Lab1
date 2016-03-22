0170a561c1a58f1d179d20078e3a2a9110da6e27
Kostez <kostelom@bk.ru>
2016-03-23 03:37:50 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <general.h>
 
+void mode_kill(int signo, int validkillpid){		
+ 		
+}		
+ 		
+void mode_pipe(){		
+ 		
+}
 int main(int argc, char** argv) {
 
     const char* short_options = "m:a::s::p::";
