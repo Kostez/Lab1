a3f17d0f3600b2ab12a615697129b1fdf832d2ee
Kostez <kostelom@bk.ru>
2016-03-23 00:09:57 +0600

    Update child.c

--- src/child.c
+++ src/child.c
@@ -6,7 +6,15 @@ void handler_child_mode(int signum, siginfo_t *info, void *f){
         printf("Код сигнала: %d\n", info->si_code);
         printf("Идентификатор процесса, пославшего сигнал: %d\n", info->si_pid);
         printf("Реальный идентификатор пользователя процесса, пославшего сигнал: %d\n", info->si_uid);
-        printf("Реальный идентификатор пользователя процесса, пославшего сигнал: %d\n", info->si_uid);
+        printf("Выходное значение или номер сигнала: %d\n", info->si_status);
+        printf("Занятое пользователем время: %li\n", info->si_utime);
+        printf("Использованное системное время: %li\n", info->si_stime);
+        printf("Значение сигнала: %d\n", info->si_value);
+        printf("Сигнал  POSIX.1b: %d\n", info->si_int);
+        printf("Сигнал  POSIX.1b: %d\n", info->si_ptr);
+        printf("Адрес в памяти, приводящий к ошибке: %d\n", info->si_addr);
+        printf("Общее событие: %d\n", info->si_band);
+        printf("Описатель файла: %d\n\n", info->si_fd);
         return;
 }
 
