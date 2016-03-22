8887887bd8b8f375dd29cf16bf0b1ac16db68da8
Kostez <kostelom@bk.ru>
2016-03-22 20:30:09 +0600

    Update general.h

--- includes/general.h
+++ includes/general.h
@@ -1,8 +1,41 @@
-#ifndef __HELLO__
-#define __HELLO__
+#ifndef __LAB1__
+#define __LAB1__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #define HELLOMSG "Say Hi!\r\n"
 
-void say_hello();
+enum Modes {
+        modes_std = 0,
+        modes_child,
+        modes_posix,
+        modes_kill,
+        modes_pipe
+}modes_e;
+
+struct Params {
+        enum Modes modes_e;
+        int amount;
+        int signalname;
+        int pid;
+}params;
+
+void worksignalss(void);
+
+void mode_std(void);
+void mode_child(void);
+void mode_posix(int amount);
+void mode_kill(int signo, int validkillpid);
+void mode_pipe(void);
+
+void handler_std_mode(int signum, siginfo_t *info, void *f);
+void handler_child_mode(int signum, siginfo_t *info, void *f);
+void handler_posix_mode(int signum, siginfo_t *info, void *f);
+void handler_kill_mode(int signum, siginfo_t *info, void *f);
+void handler_pipe_mode(int signum, siginfo_t *info, void *f);
 
 #endif
