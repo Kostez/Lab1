101ad9101bff256336219e2fec8b8467929c3203
Kostez <kostelom@bk.ru>
2016-03-31 08:53:24 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -3,7 +3,7 @@
 int signal_c = 0;
 
 void mode_posix(int n_signals) {
-	print("В mode_posix\n");
+	printf("В mode_posix\n");
 	struct sigaction sa;
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = handler_posix_mode;
