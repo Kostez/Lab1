bf546226a99dd0a5987750a0fdeec09ab2a242d4
Kostez <kostelom@bk.ru>
2016-03-31 08:29:34 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -18,10 +18,10 @@ void mode_posix(int n_signals) {
 	pid_t child;
 	int status;
 	switch(child = fork()) {
-		case -1
+		case -1:
 			perror(fork);  произошла ошибка 
 			exit(1); выход из родительского процесса
-		case 0
+		case 0:
 			srand(time(0));
 			int i=0;
 			for(; i  n_signals; i++) {
@@ -30,10 +30,10 @@ void mode_posix(int n_signals) {
 				svalue.sival_int = rand()%100;
 				random_signal = SIGRTMIN+rand()%(SIGRTMAX-1);
 				sigqueue(getppid(),random_signal,svalue);
-				printf(CHILDt%i  %i  %i  %i  %in, i, getpid(), getppid(), random_signal, svalue.sival_int);
+				printf("CHILD: \t %i | %i | %i | %i | %i\n", i, getpid(), getppid(), random_signal, svalue.sival_int);
 			}
 			break;
-		default
+		default:
 			printf(Parent PID=%d, GID=%dn, getpid(), getpgid(getpid()));
 			sleep(10);
 			if (wait(&status)  0) {
@@ -44,6 +44,6 @@ void mode_posix(int n_signals) {
 }
 
 void handler_posix_mode(int signal, siginfo_t siginfo, void context) {
-	printf(PARENTt %i  %i  %i  %i  %in, signal_c, getpid(), siginfo-si_pid, signal, siginfo-si_value.sival_int);
+	printf("PARENT\t %i | %i | %i | %i | %i\n", signal_c, getpid(), siginfo->si_pid, signal, siginfo->si_value.sival_int);
 	signal_c++;
 }
