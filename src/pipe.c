6d39289d119afa94f4d8eb8ae91993ae66886fa2
Kostez <kostelom@bk.ru>
2016-03-31 17:06:04 +0700

    Update pipe.c

--- src/pipe.c
+++ src/pipe.c
@@ -46,12 +46,12 @@ void mode_pipe(){
 			perror("close fd[0] err\n");
 			exit(1);
 		}
-		if(write(fd[1],mystring,256)==-1) {
-			perror("write err\n");
-			exit(1);
-		}
+		
 		while(1){
-			sleep(10);
+			if(write(fd[1],mystring,256)==-1) {
+				perror("write err\n");
+				exit(1);
+			}	
 		}
 	}
 }
