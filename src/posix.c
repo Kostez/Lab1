a8fd2d54a102939cb5809e635acaed8669d1847a
Kostez <kostelom@bk.ru>
2016-03-31 07:49:34 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -43,6 +43,6 @@ void mode_posix(int n_signals) {
 }
 
 void handler_posix_mode(int signal, siginfo_t *siginfo, void *context) {
-	printf("PARENT:\t %i | %i | %i | %i | %i\n", signal_c, siginfo->si_pid, getpid(), signal, siginfo->si_value.sival_int));
+	printf("PARENT:\t %i | %i | %i | %i | %i\n", signal_c, siginfo->si_pid, getpid(), signal, siginfo->si_value.sival_int);
 	signal_c++;
 }
