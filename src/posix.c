93b5ccb97b284d466ea2ddede2ba3f4e38b2516c
Kostez <kostelom@bk.ru>
2016-03-31 10:35:37 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -1,13 +1,14 @@
 #include <general.h>
 
-int signal_c = 0;
-
 void mode_posix(int n_signals) {
 	struct sigaction sa;
+	struct sigaction saChild;
 	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = handler_posix_mode;
-	printf("%i\n", SIGRTMIN);
-	printf("%i\n", SIGRTMAX);
+	sa.sa_sigaction = handler_posix_child;
+	
+	saChild.sa_flags = SA_SIGINFO;
+	saChild.sa_sigaction = handler_posix_mode;
+	
 	int i=SIGRTMIN;
 	for(; i<SIGRTMAX; i++) {
 		if(sigaction(i, &sa, 0)==-1) {
@@ -16,7 +17,12 @@ void mode_posix(int n_signals) {
 		}
 	}
 	
-	sigaction(SIGCHLD, &sa, 0);
+	sigset_t mask;
+	sa.sa_mask = mask; 
+	
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+	
+	sigaction(SIGCHLD, &saChild, 0);
 	
 	int diapozon = SIGRTMAX-SIGRTMIN;
 	
@@ -52,15 +58,16 @@ void mode_posix(int n_signals) {
 	}
 }
 
-void handler_posix_mode(int signal, siginfo_t *siginfo, void *context) {
-	if(signal == SIGCHLD){
+void handler_posix_child(int signal, siginfo_t *siginfo, void *context){
+		sigprocmask(SIG_UNBLOCK, &mask, NULL);
+		sleep(10);
 		int status = 0;
 		if (wait(&status) > 0) {
 			exit( EXIT_SUCCESS );
-		} 
-	} else {
-		fprintf(stderr,"PARENT\t %i | %i | %i | %i | %i\n", signal_c, getpid(), getppid(), signal, siginfo->si_value.sival_int);
-		signal_c++;
-	}
+		}
 }
 
+void handler_posix_mode(int signal, siginfo_t *siginfo, void *context) {
+		fprintf(stderr,"PARENT\t %i | %i | %i | %i | %i\n", signal_c, getpid(), getppid(), signal, siginfo->si_value.sival_int);
+		signal_c++;
+}
