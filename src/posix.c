0197dc213c1495e34caf781427d149aba40ae682
Kostez <kostelom@bk.ru>
2016-03-31 11:19:53 +0600

    Update posix.c

--- src/posix.c
+++ src/posix.c
@@ -6,6 +6,7 @@ char str[30][40];
 void mode_posix(int n_signals) {
 	
 	int i = 0;
+	/*
 	for(; i< 30*40; i++){
 		str[0][i] = ' ';
 	}
@@ -13,7 +14,7 @@ void mode_posix(int n_signals) {
 	for(; i< 30; i++){
 		str[0][i*40] = '\0';
 	}
-	
+*/	
 	struct sigaction sa;
 	struct sigaction saChild;
 	sa.sa_flags = SA_SIGINFO;
@@ -85,18 +86,21 @@ void handler_posix_child(int signal, siginfo_t *siginfo, void *context){
 }
 
 void handler_posix_mode(int signal, siginfo_t *siginfo, void *context) {
+	
 	int i = signal_c++;
 	
 	sigprocmask(SIG_BLOCK, &mask, NULL);
 	
+	
+/*	
 	itoa(signal_c, &str[i][0], 10);
 	itoa(getpid(), &str[i][6], 10);
 	itoa(getppid(), &str[i][12], 10);
 	itoa(signal, &str[i][18], 10);
 	itoa(siginfo->si_value.sival_int, &str[i][24], 10);
 	
-	sigprocmask(SIG_UNBLOCK, &mask, NULL);
-//	fprintf(stderr,"PARENT\t %i | %i | %i | %i | %i\n", signal_c, getpid(), getppid(), signal, siginfo->si_value.sival_int);
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);*/
+	sprintf(str[i], "PARENT\t %i | %i | %i | %i | %i\n", signal_c, getpid(), getppid(), signal, siginfo->si_value.sival_int);
 //	signal_c++;
 
 }
